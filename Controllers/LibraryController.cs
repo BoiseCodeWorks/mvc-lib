35d4e3fddd9097d9cba85b9b81ea64b4c5e185b6
Jake Overall <joverall22@gmail.com>
2016-11-29 12:19:40 -0700

    Adds Books to Library

--- Controllers/LibraryController.cs
+++ Controllers/LibraryController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using mvc_library.Models;
+using Microsoft.EntityFrameworkCore;
+
 
 namespace mvc_library.Controllers
 {
@@ -36,32 +38,32 @@ namespace mvc_library.Controllers
 
         public IActionResult Details(int id)
         {
-            var library = _db.Libraries.Find(id);
+            var library = _db.Libraries.Include(
+              db => db.Books
+            ).FirstOrDefault(lib => lib.Id == id);
 
             if (library == null)
             {
                 return NotFound($"Sorry there is no library by that id: {id}");
             }
-
             return View(library);
-
         }
 
-        // public IActionResult AddBook()
-        // {
-        //     return View();
-        // }
+        [HttpGet("/Library/Details/{libraryId}/Books")]
+        public IActionResult AddBook(int libraryId)
+        {
+            return View();
+        }
 
-        // [HttpPost]
-        // public IActionResult AddBook(string title, string author, string description)
-        // {
-        //     if (title == null || author == null)
-        //     {
-        //         return RedirectToAction("AddBook");
-        //     }
-        //     library.AddBook(new Book(title, author, description));
-        //     return RedirectToAction("Index");
-        // }
+        [HttpPost("/Library/Details/{libraryId}/Books")]
+        public IActionResult AddBook(int libraryId, Book book)
+        {
+            book.LibraryId = libraryId;
+            _db.Books.Add(book);
+            _db.SaveChanges();
+            return RedirectToAction("Details", new { id = libraryId });
+        }
+
         // public IActionResult ViewBook(int id)
         // {
         //   var myBook = library.GetBookById(id);
